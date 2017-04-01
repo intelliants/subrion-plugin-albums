23ade6ba8d156e77fb6d425dd08536f670e0a849
Vasily Bezruchkin <vbezruchkin@intelliants.com>
2017-04-01 20:15:29 +0600

    Updated

--- js/frontend/album.js
+++ js/frontend/album.js
@@ -1,56 +1,54 @@
-$(function()
+$(function ()
 {
-	var $link = $('#move-link');
-	var move_link = $link.attr('href');
+    var $link = $('#move-link');
+    var move_link = $link.attr('href');
 
-	$('#create-album, #upload-photo, #move-photos').appendTo('body');
+    $('#create-album, #upload-photo, #move-photos').appendTo('body');
 
-	$link.attr('href', '');
-	$('#move-to').val($('#modal-move-to').val());
+    $link.attr('href', '');
+    $('#move-to').val($('#modal-move-to').val());
 
-	$('#remove-album').on('click', function(e)
-	{
-		confirm(_f('album_del_confirm')) || e.preventDefault();
-	});
+    $('#remove-album').on('click', function (e)
+    {
+        confirm(_f('album_del_confirm')) || e.preventDefault();
+    });
 
-	$('#upload-photo-btn').on('click', function(e)
-	{
-		$(this).addClass('disabled').html('<span class="fa fa-refresh"></span>');
-	});
+    $('#upload-photo-btn').on('click', function (e)
+    {
+        $(this).addClass('disabled').html('<span class="fa fa-refresh"></span>');
+    });
 
-	$('#remove-photo').on('click', function(e)
-	{
-		return confirm(_f('rm_photo'));
-	});
+    $('#remove-photo').on('click', function (e)
+    {
+        return confirm(_f('rm_photo'));
+    });
 
-	$('#move-photo').on('click', function(e)
-	{
-		$('input[name="move"]').click();
-	});
+    $('#move-photo').on('click', function (e)
+    {
+        $('input[name="move"]').click();
+    });
 
-	$('#modal-move-to').on('change', function()
-	{
-		$('#move-to').val($(this).val());
-	});
+    $('#modal-move-to').on('change', function ()
+    {
+        $('#move-to').val($(this).val());
+    });
 
-	$('input[name="checked[]"]').on('change', function(e)
-	{
-		e.preventDefault();
+    $('input[name="checked[]"]').on('change', function (e)
+    {
+        e.preventDefault();
 
-		var selectedImagesCount = $('input[name="checked[]"]:checked').length;
+        var selectedImagesCount = $('input[name="checked[]"]:checked').length;
 
-		if (selectedImagesCount > 0)
-		{
-			$('#remove-photo').prop('disabled', false);
+        if (selectedImagesCount > 0) {
+            $('#remove-photo').prop('disabled', false);
 
-			$link.prop('disabled', false).attr('href', move_link);
-			$('#buttoncover').prop('disabled', 1 != selectedImagesCount);
-		}
-		else
-		{
-			$('#remove-photo, #buttoncover').prop('disabled', true);
+            $link.prop('disabled', false).attr('href', move_link);
+            $('#buttoncover').prop('disabled', 1 != selectedImagesCount);
+        }
+        else {
+            $('#remove-photo, #buttoncover').prop('disabled', true);
 
-			$link.prop('disabled', true).attr('href', '');
-		}
-	});
+            $link.prop('disabled', true).attr('href', '');
+        }
+    });
 });
